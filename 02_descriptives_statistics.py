7ba684754e43391d0eb028b083da1ead9a4a6a7e
Yassir CHAOUB <Chaoubyassir@gmail.com>
2026-04-30 15:21:40 +0200

    Load data

--- 02_descriptives_statistics.py
+++ 02_descriptives_statistics.py
@@ -1,4 +1,7 @@
+import pandas as pd
+INPUT = "/neurospin/signatures/2026_ychaoub_rlink_predict_response_clinic/data/"
 # % Load data
+Clinical_df = pd.read_excel(INPUT + 'Clinical_data.xlsx')
 
 # % Univariate statistics
 
